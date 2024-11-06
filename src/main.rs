97399032e0ebceebd958fa94fcefb6aed29b8a33
rust_lang_lover <rust_lang_lover@proton.me>
2024-11-06 12:55:18 +0300

    // started implementing handle_connection()

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use std::net::SocketAddr;
-use log::info;
-use tokio::net::TcpListener;
+use log::{error, info};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_async, WebSocketStream};
+use tokio_tungstenite::tungstenite::Error;
+
+async fn handle_connection(stream : TcpStream) -> () {
+    // Accept the websocket connection
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(err) => {
+            error!("Error during the websocket handshake : {}", err);
+            return;
+        }
+    };
+
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> () {
     let listener =
         TcpListener::bind("0.0.0.0:8000".parse::<SocketAddr>().unwrap())
             .await

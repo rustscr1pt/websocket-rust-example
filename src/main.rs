775158a07ed503b137523c67e4458802f8cd10e5
rust_lang_lover <rust_lang_lover@proton.me>
2024-11-06 12:50:32 +0300

    //

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use log::info;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener =
+        TcpListener::bind("0.0.0.0:8000".parse::<SocketAddr>().unwrap())
+            .await
+            .expect("Couldn't bind.");
+    info!("Listening on : http://localhost:8000");
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream))
+    }
 }
